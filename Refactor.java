1f24f2b280a971c0ae17c938a15fe05acbf88c3c
halima154 <afrozhalima71@gmail.com>
2017-04-08 19:23:27 -0700

    Add files via upload
    commit - printBanner method

---
+++ Refactor.java
@@ -0,0 +1,29 @@
+package refactoring;
+
+public class Refactor{
+	private MyCollection _orders = new MyCollection();
+	private String _name = "Customer Name";
+	
+	void printOwing(){
+		Enumeration e = _orders.elements();
+		double outstanding = 0.0;
+		
+		printBanner();
+	}
+
+	
+	while(e.hasMoreElements()){
+			Order each = (Order) e.nextElement();
+			outstanding += each.getAmount();	
+	}
+	return outstanding;
+
+	System.out.println("name: " + _name);
+	System.out.println("amount: " + outstanding);
+
+	public void printBanner() {
+		System.out.println("***************************");
+		System.out.println("***** Customer Owes *******");
+		System.out.println("***************************");
+	}
+}
